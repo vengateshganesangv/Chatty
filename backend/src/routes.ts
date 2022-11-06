7076b4a5c460637b92ad50a56a28985112d873c7
VengateshGanesan <vengateshtendulkar@gmail.com>
2022-11-06 12:41:15 +0530

    Initial backend reaction commit

---
+++ backend/src/routes.ts
@@ -0,0 +1,27 @@
+import { Application } from 'express';
+import { authRoutes } from '@auth/routes/authRoutes';
+import { serverAdapter } from '@service/queues/base.queue';
+import { postRoutes } from '@post/routes/postRoutes';
+import { currentUserRoutes } from '@auth/routes/currentRoutes';
+import { authMiddleware } from '@global/helpers/auth-middleware';
+import { healthRoutes } from '@user/routes/healthRoutes';
+import { reactionRoutes } from '@reaction/routes/reactionRoutes';
+
+const BASE_PATH = '/api/v1';
+export default (app: Application) => {
+  const routes = () => {
+    app.use('/queues', serverAdapter.getRouter());
+
+    app.use('', healthRoutes.health());
+    app.use('', healthRoutes.env());
+    app.use('', healthRoutes.instance());
+    app.use('', healthRoutes.fiboRoutes());
+
+    app.use(BASE_PATH, authRoutes.routes());
+    app.use(BASE_PATH, authRoutes.signoutRoute());
+    app.use(BASE_PATH, authMiddleware.verifyUser, currentUserRoutes.routes());
+    app.use(BASE_PATH, authMiddleware.verifyUser, postRoutes.routes());
+    app.use(BASE_PATH, authMiddleware.verifyUser, reactionRoutes.routes());
+  };
+  routes();
+};
