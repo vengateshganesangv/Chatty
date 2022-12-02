7d57c864825c1d46fbf4781deea3f7f6b181c8af
VengateshGanesan <vengateshtendulkar@gmail.com>
2022-12-02 13:50:24 +0530

    user extra feature added

--- backend/src/shared/services/db/user.service.ts
+++ backend/src/shared/services/db/user.service.ts
@@ -1,7 +1,9 @@
-import { IUserDocument } from '@user/interfaces/user.interface';
+import { IBasicInfo, IUserDocument, ISearchUser, ISocialLinks, INotificationSettings } from '@user/interfaces/user.interface';
 import { UserModel } from '@user/models/user.schema';
 import mongoose from 'mongoose';
 import { AuthModel } from '@auth/models/auth.schema';
+import { followerService } from '@service/db/follower.service';
+import { indexOf } from 'lodash';
 
 class UserService {
   public async addUserData(data: IUserDocument): Promise<void> {
@@ -10,6 +12,33 @@ class UserService {
 
   public async updatePassword(username: string, hashedPassword: string): Promise<void> {
     await AuthModel.updateOne({ username }, { $set: { password: hashedPassword } }).exec();
+  }
+
+  public async updateUserInfo(userId: string, info: IBasicInfo): Promise<void> {
+    await UserModel.updateOne(
+      { _id: userId },
+      {
+        $set: {
+          work: info['work'],
+          school: info['school'],
+          quote: info['quote'],
+          location: info['location']
+        }
+      }
+    ).exec();
+  }
+
+  public async updateSocialLinks(userId: string, links: ISocialLinks): Promise<void> {
+    await UserModel.updateOne(
+      { _id: userId },
+      {
+        $set: { social: links }
+      }
+    ).exec();
+  }
+
+  public async updateNotificationSettings(userId: string, settings: INotificationSettings): Promise<void> {
+    await UserModel.updateOne({ _id: userId }, { $set: { notifications: settings } }).exec();
   }
 
   public async getUserById(userId: string): Promise<IUserDocument> {
@@ -31,7 +60,73 @@ class UserService {
     ]);
     return users[0];
   }
+  public async getAllUsers(userId: string, skip: number, limit: number): Promise<IUserDocument[]> {
+    const users: IUserDocument[] = await UserModel.aggregate([
+      { $match: { _id: { $ne: new mongoose.Types.ObjectId(userId) } } },
+      { $skip: skip },
+      { $limit: limit },
+      { $sort: { createdAt: -1 } },
+      { $lookup: { from: 'Auth', localField: 'authId', foreignField: '_id', as: 'authId' } },
+      { $unwind: '$authId' },
+      { $project: this.aggregateProject() }
+    ]);
+    return users;
+  }
+  public async getRandomUsers(userId: string): Promise<IUserDocument[]> {
+    const randomUsers: IUserDocument[] = [];
+    const users: IUserDocument[] = await UserModel.aggregate([
+      { $match: { _id: { $ne: new mongoose.Types.ObjectId(userId) } } },
+      { $lookup: { from: 'Auth', localField: 'authId', foreignField: '_id', as: 'authId' } },
+      { $unwind: '$authId' },
+      { $sample: { size: 10 } },
+      {
+        $addFields: {
+          username: '$authId.username',
+          email: '$authId.email',
+          avatarColor: '$authId.avatarColor',
+          uId: '$authId.uId',
+          createdAt: '$authId.createdAt'
+        }
+      },
+      {
+        $project: {
+          authId: 0,
+          __v: 0
+        }
+      }
+    ]);
+    const followers: string[] = await followerService.getFolloweesIds(`${userId}`);
+    for (const user of users) {
+      const followerIndex = indexOf(followers, user._id.toString());
+      if (followerIndex < 0) {
+        randomUsers.push(user);
+      }
+    }
+    return randomUsers;
+  }
 
+  public async getTotalUsersInDB(): Promise<number> {
+    const totalCount: number = await UserModel.find({}).countDocuments();
+    return totalCount;
+  }
+
+  public async searchUsers(regex: RegExp): Promise<ISearchUser[]> {
+    const users = await AuthModel.aggregate([
+      { $match: { username: regex } },
+      { $lookup: { from: 'User', localField: '_id', foreignField: 'authId', as: 'user' } },
+      { $unwind: '$user' },
+      {
+        $project: {
+          _id: '$user._id',
+          username: 1,
+          email: 1,
+          avatarColor: 1,
+          profilePicture: 1
+        }
+      }
+    ]);
+    return users;
+  }
   private aggregateProject() {
     return {
       _id: 1,
